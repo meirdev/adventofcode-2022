054e683ded287174c72f7612db87c88ecc15c979
Meir <maornet@gmail.com>
2024-06-03 07:20:51 +0300

    day10

---
+++ day10/main.py
@@ -0,0 +1,93 @@
+import dataclasses
+import itertools
+from typing import Iterator
+
+
+@dataclasses.dataclass
+class Instruction:
+    pass
+
+
+@dataclasses.dataclass
+class addx(Instruction):
+    value: int
+
+
+@dataclasses.dataclass
+class noop(Instruction):
+    pass
+
+
+def parse_input(input: str) -> list[Instruction]:
+    instructions: list[Instruction] = []
+
+    for line in input.strip().splitlines():
+        parts = line.split(" ")
+
+        match parts:
+            case ["noop"]:
+                instructions.append(noop())
+            case ["addx", value]:
+                instructions.append(addx(int(value)))
+
+    return instructions
+
+
+def run(input: str) -> Iterator[tuple[int, int]]:
+    instructions = parse_input(input)
+
+    cycles, x = 0, 1
+
+    for i in instructions:
+        match i:
+            case noop():
+                cycles += 1
+                yield cycles, x
+            case addx(value):
+                cycles += 1
+                yield cycles, x
+                cycles += 1
+                yield cycles, x
+                x += value
+
+
+def part1(input: str) -> int:
+    ticks = itertools.chain([20], itertools.count(60, 40))
+
+    sum_signal_strength, tick = 0, next(ticks)
+
+    for cycles, x in run(input):
+        if cycles == tick:
+            sum_signal_strength += cycles * x
+            tick = next(ticks)
+
+    return sum_signal_strength
+
+
+def part2(input: str) -> str:
+    ticks = itertools.count(40, 40)
+
+    output, column, tick = "", 0, next(ticks)
+
+    for cycles, x in run(input):
+        output += "#" if x - 1 <= column <= x + 1 else "."
+        column += 1
+
+        if cycles == tick:
+            output += "\n"
+            column, tick = 0, next(ticks)
+
+    return output.strip()
+
+
+def main() -> None:
+    with open("input.txt") as file:
+        input = file.read()
+
+    print("Part 1:", part1(input))
+    print("Part 2:")
+    print(part2(input))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day10/test_main.py
@@ -0,0 +1,182 @@
+from .main import part1, part2
+
+
+INPUT = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+
+
+def test_part1():
+    assert part1(INPUT) == 13140
+
+
+def test_part2():
+    assert part2(INPUT) == """
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+""".strip()
+
+
+def test_input():
+    with open("input.txt") as file:
+        input = file.read()
+
+    assert part1(input) == 14760
+    # EFGERURE
+    assert part2(input) == """
+####.####..##..####.###..#..#.###..####.
+#....#....#..#.#....#..#.#..#.#..#.#....
+###..###..#....###..#..#.#..#.#..#.###..
+#....#....#.##.#....###..#..#.###..#....
+#....#....#..#.#....#.#..#..#.#.#..#....
+####.#.....###.####.#..#..##..#..#.####.
+""".strip()
