9df7a19f2d886f617b5653e8c656beb9b0568999
Meir <maornet@gmail.com>
2024-06-05 17:50:38 +0300

    day12 - main

--- day12/main.py
+++ day12/main.py
@@ -1,5 +1,7 @@
 from typing import NamedTuple
 
+import networkx as nx
+
 
 class Position(NamedTuple):
     y: int
@@ -38,12 +40,41 @@ def parse_input(input: str) -> HeightMap:
     return HeightMap(map, start, end)
 
 
+def solution(input: str, all_nodes: bool) -> int:
+    height_map = parse_input(input)
+
+    G: nx.DiGraph[Position] = nx.DiGraph()
+
+    for pos in height_map.map:
+        for y, x in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            pos_ = Position(pos[0] + y, pos[1] + x)
+
+            if (
+                pos_ in height_map.map
+                and height_map.map[pos] + 1 >= height_map.map[pos_]
+            ):
+                G.add_edge(pos, pos_)
+
+    if all_nodes:
+        nodes = [pos for pos, value in height_map.map.items() if value == 0]
+    else:
+        nodes = [height_map.start]
+
+    def get_path_length(position: Position) -> int | float:
+        try:
+            return nx.dijkstra_path_length(G, position, height_map.end)
+        except nx.exception.NetworkXNoPath:
+            return float("inf")
+
+    return min(get_path_length(i) for i in nodes)
+
+
 def part1(input: str) -> int:
-    pass
+    return solution(input, all_nodes=False)
 
 
 def part2(input: str) -> int:
-    pass
+    return solution(input, all_nodes=True)
 
 
 def main() -> None:
